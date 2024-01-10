e01a24b95aa9c68d8a6aa4e87e61a3bf269c440d
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2024-01-10 19:33:33 +0200

    Завдання 4: Вибірка данихНапишіть запит, щоб вивести всіх студентів у таблиці Students.

--- practical_lesson2.sql
+++ practical_lesson2.sql
@@ -1,13 +1,2 @@
-CREATE TABLE Students(
-student_id SERIAL PRIMARY KEY,
-student_name VARCHAR(50),
-age INT,
-email VARCHAR(50)
-);
-INSERT INTO Students (student_name, age, email) 
-VALUES 
-    ('John Doe', 20, 'john@example.com'),
-    ('Alice Smith', 22, 'alice@example.com'),
-    ('Bob Johnson', 21, 'bob@example.com'),
-    ('Emily Davis', 19, 'emily@example.com'),
-    ('Michael Wilson', 23, 'michael@example.com')
+-- Завдання 4: Вибірка данихНапишіть запит, щоб вивести всіх студентів у таблиці "Students".
+SELECT * FROM Students;
