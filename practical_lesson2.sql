46b6f236767ea29859895ccb85a291eb89d50351
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2024-01-10 19:31:37 +0200

    Завдання 2: Створення таблиці

--- practical_lesson2.sql
+++ practical_lesson2.sql
@@ -1,2 +1,13 @@
---Завдання 1: Створення бази даних Створіть нову базу даних з назвою "SchoolDB".
-CREATE DATABASE "SchoolDB"
+CREATE TABLE Students(
+student_id SERIAL PRIMARY KEY,
+student_name VARCHAR(50),
+age INT,
+email VARCHAR(50)
+);
+INSERT INTO Students (student_name, age, email) 
+VALUES 
+    ('John Doe', 20, 'john@example.com'),
+    ('Alice Smith', 22, 'alice@example.com'),
+    ('Bob Johnson', 21, 'bob@example.com'),
+    ('Emily Davis', 19, 'emily@example.com'),
+    ('Michael Wilson', 23, 'michael@example.com')
